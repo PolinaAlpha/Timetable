76a2b57afd33ff3f6edafc0dc0d820257f37c284
PolinaAlpha <94176008+PolinaAlpha@users.noreply.github.com>
2021-11-13 01:47:22 +0900

    Authpage

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,43 @@
 <template lang='pug'>
-div.flex.bg-black dads
+div.w-full.h-full.grid.grid-cols-2.mt-8
+    div.ml-auto.pt-24.pb-24.px-20.text-center.grid.grid-cols-1.gap-4.glassmor
+        div.p-4.inputlog.m-2 Авторизация
+        div.p-4.gap-4.grid.grid-cols-4
+            span.text-left.mr-2.self-center Логин
+            input.bg-current.inputlog.col-span-3.p-1.border-red-500
+            span.text-left.mr-2.self-center Пароль
+            input.bg-current.inputlog.col-span-3.p-1.border-red-500
+        div.flex.mx-auto
+            button.py-4.bg-pink-300.px-8.mr-8.rounded-xl Войти
+            button.text-gray-700 Зарегистрироваться
+    img.col-start-2(src="./assets/ZKB7S3Z0_Vw.jpg")
 </template>
-
+<script> 
+</script>
 <style lang="postcss">
 @import "./assets/styles/tailwind.postcss";
+html{
+    background-image: linear-gradient(120deg, #fccb90 0%, #d57eeb 100%);
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+}
+.center{
+    margin:10%;
+}
+.glassmor{
+background: rgba( 255, 255, 255, 0.45 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 0px );
+-webkit-backdrop-filter: blur( 0px );
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+}
+.inputlog{
+/* background: #e0e0e0; */
+background: rgba(255, 255, 255, 0.363);
+box-shadow: 0 8px 10px 0 rgba(0, 0, 0, 0.062);
+backdrop-filter: blur( 10.0px );
+-webkit-backdrop-filter: blur( 10.0px );
+border-radius: 10px;
+}
 </style>
